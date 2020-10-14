40fbd226f3a2daad0a5c973f8e50aafa97c894e2
Erik Zenker <erikzenker@posteo.de>
2020-10-14 22:48:02 +0200

    transform lambda tuple to typeid tuple

---
+++ src/include/hsm/details/utils/toTypeid.h
@@ -0,0 +1,10 @@
+#include <boost/hana/transform.hpp>
+#include <boost/hana/type.hpp>
+
+namespace hsm::details::utils {
+
+auto toTypeid = [](auto tuple){
+    return boost::hana::transform(tuple, boost::hana::typeid_);
+};
+
+}
